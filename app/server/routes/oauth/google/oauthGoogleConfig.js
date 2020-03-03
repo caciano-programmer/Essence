54030d8c5a3f3b3e9228e8c89fa10bcf12cb8631
caciano almeida <oalme004@fiu.edu>
2020-03-03 18:09:05 -0500

    refactored google config file

---
+++ app/server/routes/oauth/google/oauthGoogleConfig.js
@@ -0,0 +1,12 @@
+/*
+    Your secret config data goes here.
+    You must register your application with google through googles developer console.
+    For more information go to: https://developers.google.com/identity/protocols/OAuth2
+    Look at example below for reference 
+
+    export const oauthSecretGoogleConfig = {
+        client_id: your_client_id,
+        client_secret: your_client_secret,
+        redirect_uri: 'url that google will send response to, e.g. http://localhost:4000/oauth/google/response'
+    }
+*/
