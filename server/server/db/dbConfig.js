618a97ca3d5d9b45362fc2f00352a9a688dfb447
caciano almeida <oalme004@fiu.edu>
2020-03-25 15:32:36 -0400

    changed database development to empty password

--- server/server/db/dbConfig.js
+++ server/server/db/dbConfig.js
@@ -5,7 +5,7 @@ const config = {
   connection: {
     host: process.env.HOST || '127.0.0.1',
     user: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'password',
+    password: process.env.NODE_ENV === 'production' ?  process.env.DB_PASSWORD : '',
     database: process.env.NODE_ENV === 'production' ? 'Essence' : 'Essence_Test',
   },
   pool: { min: 2, max: 10 },
